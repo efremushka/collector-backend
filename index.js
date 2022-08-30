4e031e8f29a5ddff9d7d1d768649e92a16302da1
Ann <efremenkova9533@gmail.com>
2022-08-30 10:50:14 +0300

    fixed mongo

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import { handleValidationErrors, checkAuth } from "./utils/index.js"
 import { UserController, CollectionController } from "./controllers/index.js"
 
 mongooose
-    .connect(process.env.MONGODB)
+    .connect(process.env.MONGODB_URI)
     .then(() => console.log("MongoDB ok"))
     .catch((err) => console.log("MongoDB error, err"))
 
